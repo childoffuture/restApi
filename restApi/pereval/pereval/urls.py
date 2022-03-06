d9dc4af6ad3d9c4968772c446bfad1539abcfa54
ChildOfFuture <dimp89@mail.ru>
2022-03-06 17:56:22 +0300

    rm all

--- restApi/pereval/pereval/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-   path('submitData/', PerevalView.as_view()),
-   path('submitData/<int:pk>', PerevalRecordView.as_view()),
-   path('submitData/<int:pk>/status', PerevalStatusView.as_view()),
-]
